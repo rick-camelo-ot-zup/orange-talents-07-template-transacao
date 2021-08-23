6cbfa31eda15b0d90aa229979274ee9169f4d04e
Rick Camelo Martins <rick.martins@zup.com.br>
2021-08-23 12:30:06 -0300

    primeiro commit

---
+++ src/main/java/br/rickcm/transacaoapp/TransacaoAppApplication.java
@@ -0,0 +1,13 @@
+package br.rickcm.transacaoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/rickcm/transacaoapp/TransacaoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rickcm.transacaoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
